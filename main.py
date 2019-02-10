8a07ec1d973b21535944b4cb6c6f5eca7191043e
VegaAce <zvega7596@colliervilledragons.org>
2019-02-10 15:43:35 -0600

    the pygame mapping is currently wrong along with a minor bug in the algorithm

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 import pygame
-from aStar import *
+import numpy as np
+import time
+from aStar import astar
 
+maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 1, 1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
+start = (0,9)
+end = (9,0)
 resolution = (1000, 1000)
-
+white = [255,255,255]
+black = [0,0,0]
 pygame.init()
 screen = pygame.display.set_mode(resolution)
 background = pygame.Surface(resolution)
-pygame.display.flip
+pygame.display.flip()
+path = astar(maze, start, end )
+pygamePath = np.array(path)
+pygamePath = pygamePath * 100
+print(path)
 while(True):
-    screen.blit(background,(0,0))
+    event = pygame.event.get()
+    for event in event:
+        if event.type == pygame.QUIT:
+            pygame.QUIT()
+
+    thing = len(pygamePath)
+    i = 0
+    for x in range(thing):
+        background.fill(white)
+
+        player = pygame.draw.rect(background, black, ((pygamePath[i][0], pygamePath[i][1]),(10,10)))
+
+        i += 1
+        screen.blit(background,(0,0))
+        pygame.display.update()
+        time.sleep(1)
